35116d82e904a50f6bd1b35017f1db5307da0c3c
v.aristarkhova <Veronika.Aristarkhova@acronis.com>
2022-09-12 21:23:36 +0300

    Added nickname logic

---
+++ client/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(conn)
+	go func() {
+		_, err := io.Copy(os.Stdout, conn)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+	_, err = io.Copy(conn, os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	} // until you send ^Z
+	fmt.Printf("%s: exit", conn.LocalAddr())
+}
