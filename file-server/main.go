6b44f539c9232f1c098ffe6352f05b1e161e4045
v.aristarkhova <Veronika.Aristarkhova@acronis.com>
2022-10-06 19:56:58 +0300

    Add posibility to receive file list from upload directory

---
+++ file-server/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type Employee struct {
+	Name   string  `json:"name" xml:"name"`
+	Age    int     `json:"age" xml:"age"`
+	Salary float32 `json:"salary" xml:"salary"`
+}
+
+type Handler struct {
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		name := r.FormValue("name")
+		fmt.Fprintf(w, "Parsed query-param with key \"name\": %s", name)
+	case http.MethodPost:
+		var employee Employee
+		contentType := r.Header.Get("Content-Type")
+		switch contentType {
+		case "application/json":
+			err := json.NewDecoder(r.Body).Decode(&employee)
+			if err != nil {
+				http.Error(w, "Unable to unmarshal JSON", http.StatusBadRequest)
+				return
+			}
+		case "application/xml":
+			err := xml.NewDecoder(r.Body).Decode(&employee)
+			if err != nil {
+				http.Error(w, "Unable to unmarshal XML", http.StatusBadRequest)
+				return
+			}
+		default:
+			http.Error(w, "Unknown content type", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(w, "Got a new employee!\nName: %s\nAge: %dy.o.\nSalary %0.2f\n",
+			employee.Name,
+			employee.Age,
+			employee.Salary,
+		)
+	}
+}
+
+func main() {
+	handler := &Handler{}
+	http.Handle("/", handler)
+	srv := &http.Server{
+		Addr:         ":80",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	srv.ListenAndServe()
+}
